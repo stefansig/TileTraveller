f6c1585cb15eeda3691a3cc6b4ac67ce83f33ee0
Helena Hanna Guðlaugsdóttir <Helena@Helenas-MacBook-Air-2.local>
2020-09-17 10:19:33 +0000

    first change

--- tile_traveller.py
+++ tile_traveller.py
@@ -1,1 +1,19 @@
 #test
+#Player starts at position 1,1.
+
+#1,1 : n
+#1,2 : n / e / s
+#1,3 : s / e
+#2,1 : n
+#2,2 : s / w
+#2,3 : w / e
+#3,1 : n victory
+#3,2 : s / n
+#3,3 : s / w
+
+
+print('You can travel: ')
+direction = input('Direction: ')
+print('Not a valid direction!')
+print('Victory!')
+
