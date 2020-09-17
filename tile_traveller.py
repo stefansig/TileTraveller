d1813260eddb6e3506b7ef42d8f5d12591e6339d
Stefan Sigurjonsson <stefans20@ru.is>
2020-09-17 20:15:06 +0000

    Klára verkefni

--- tile_traveller.py
+++ tile_traveller.py
@@ -7,6 +7,8 @@
 #boðið honum rétta möguleika.
 #leikmaður byrjar í reit 1.1
 
+#https://github.com/stefansig/TileTraveller/blob/master/tile_traveller.py
+
 POSITION = 1.1
 
 
@@ -42,8 +44,8 @@ def options(position):
         print("You can travel: (N)orth.")
         return "n"
     elif position == 2.2:
-        print("You can travel: (N)orth or (S)outh or (W)est.")
-        return "nsw"
+        print("You can travel: (S)outh or (W)est.")
+        return "sw"
     elif position == 2.3:
         print("You can travel: (E)ast or (W)est.")
         return "ew"
@@ -59,9 +61,6 @@ def options(position):
 
 
 
-
-
-
 while POSITION != 3.1:
     legal = options(POSITION)
     direction = input('Direction: ').lower()
@@ -70,33 +69,11 @@ while POSITION != 3.1:
     else:
         print("Not a valid direction!")
 
-
-
-#present_position = new_position(direction)
-#options = options(present_position)
-
-
-#print('You can travel: ')
-#direction = input('Direction: ')
-#print('Not a valid direction!')
-
 print('Victory!')
 
 
 
 
-
-#Player starts at position 1,1.
-
-#1,1 : n
-#1,2 : n / e / s
-#1,3 : s / e
-#2,1 : n
-#2,2 : s / w
-#2,3 : w / e
-#3,1 : n victory
-#3,2 : s / n
-#3,3 : s / w
 
 
 
